44ed973abf03a46eb48f802c8780d6a0ced86dc1
Marcelo Elizondo Gzz <Personal@MacBookPro.lan1>
2025-12-09 23:05:08 -0800

    added photo

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ const imageFiles = [
   "DSC06326.jpg",
   "DSC07410.jpg",
   "DSC08414.jpg",
-  "_DSC0776-Pano_copy_2.jpg"
+  "_DSC0776-Pano_copy_2.jpeg"
 ];
 
 // Optional captions for images shown in the lightbox.
